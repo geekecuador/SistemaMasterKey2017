6b62c8e96f52bc2f1ec479544b803e266c08c669
dpulloquinga <dapl@me.com>
2017-09-28 02:11:42 -0500

    port gmail

--- Sistema/settings.py
+++ Sistema/settings.py
@@ -164,7 +164,7 @@ DEFAULT_FILE_STORAGE = 'Sistema.storage_backends.MediaStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'treelab@coworkinglatacunga.com'
 EMAIL_HOST_PASSWORD = 'D@vidpull01234555'
 EMAIL_USE_TLS = True
