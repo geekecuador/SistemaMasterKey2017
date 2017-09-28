d26e651ead69dbac4796bd920765f4ea4b895363
dpulloquinga <dapl@me.com>
2017-09-28 00:10:37 -0500

    b2 Santo Domingo

--- user.py
+++ user.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 import csv
 from masterkey.models import Estudiante, Programa, Ciudad, Nivel
 
-f = open('b1SantoDomingo.csv', 'r')
+f = open('b2SantoDomingo.csv', 'r')
 with f:
 	reader = csv.DictReader(f)
 	for row in reader:
