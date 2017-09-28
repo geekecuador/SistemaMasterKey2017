e33452d32afb0741e1bd47dd721dd0ddab5d2c0a
dpulloquinga <dapl@me.com>
2017-09-27 20:28:57 -0500

    change numbers in script

--- user.py
+++ user.py
@@ -6,17 +6,16 @@ f = open('a1SantoDomingo.csv', 'r')
 with f:
 	reader = csv.DictReader(f)
 	for row in reader:
-		print row['usuario']
-		print row['usuario']
+		print row[0]
 		try:
-			user = User.objects.create_user(row['usuario'], password=row['password'], email=row['email'])
+			user = User.objects.create_user(row[4], password=row[5], email=row[6])
 			user.is_superuser=False
 			user.is_staff=False
 			user.save()
 			try:
-				estudiante = Estudiante(cedula='0500995972',usuario=User.objects.get(username=row['usuario']),
+				estudiante = Estudiante(cedula=row[0],usuario=User.objects.get(username=row[4]),
 					programa=Programa.objects.get(id=1),ciudad=Ciudad.objects.get(nombre='Santo Domingo'),nivel=Nivel.objects.get(id=1),
-					fecha_nacimiento=row['usuario'], telefono = '0999999999',direccion_domicilio='ninguna', fecha_de_inicio=row['inicio'],fecha_de_expiracion=row['terminacion'])
+					fecha_nacimiento=row[5], telefono = '0999999999',direccion_domicilio='ninguna', fecha_de_inicio=row[1],fecha_de_expiracion=row[2])
 			except Exception as e:
 				print "Error en el estudiante"
 		except Exception as e:
