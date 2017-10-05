15d03d16f1ad871ddc032c98ab59fd3ce5d0744d
dpulloquinga <dapl@me.com>
2017-10-04 19:29:20 -0500

    add report

--- masterkey/views.py
+++ masterkey/views.py
@@ -344,7 +344,8 @@ def exportar_estudiantes_xls(estado, ciudad):
                                                          'contrato__fecha_creacion', 'contrato__duracion',
                                                          'fecha_nacimiento', 'telefono',
                                                          'Estudiante__estado__seguimiento__comentario').filter(
-        Estudiante__estado_id=estado).filter(ciudad_id=ciudad)
+        Estudiante__estado_id=estado).filter(ciudad_id=ciudad).values('usuario__email')
+
     # .values_list('user', 'first_name', 'last_name', 'email')
     # rows = User.objects.values()
     for row in estudiantes_activos:
