d2b636ca6b01d1f8824543d82c2c403f00f7765f
David Pulloquinga <dapulloqquinga@espe.edu.ec>
2017-10-10 17:33:09 -0500

    limitaciones

--- masterkey/views.py
+++ masterkey/views.py
@@ -102,11 +102,10 @@ def paso1(request):
         username = request.user
         estudiante = Estudiante.objects.get(usuario=username)
         academico = Academic_Rank.objects.filter(nivel_id=999).filter(estudiante=estudiante)
-
         dt = datetime.datetime.now()
         start = dt -datetime.timedelta(days=dt.weekday())
         end = start + datetime.timedelta(days=6)
-        limitacion = Limitaciones.objects.filter(fecha_reserva__range=[start,end])
+        limitacion = Limitaciones.objects.filter(fecha_reserva__range=[start,end]).filter(estudiante=estudiante)
 
         if len(academico) >= 1:
             return render(request, 'consulta1pasivo.html', {'username': username, 'estudiante': estudiante})
