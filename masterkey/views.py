05003467a32fce2351c28f320865b2c0699fef5e
David Pulloquinga <dapulloqquinga@espe.edu.ec>
2017-10-17 10:07:32 -0500

    changes

--- masterkey/views.py
+++ masterkey/views.py
@@ -423,7 +423,7 @@ def exportar_cursos_xls(fecha, sede):
         ws.write(row_num, 2, x.tipo_nivel, font_style)
         b = ""
         for a in x.estudiantes.prefetch_related('alumnos'):
-            a = a.usuario.get_full_name() + ' ' + a.nivel.tema
+            a = a.usuario.get_full_name() + ' ' + str(a.nivel.pk)
             b = b + ' ' + str(a) + '| '
             print(b)
         ws.write(row_num, 3, b, font_style)
