ceabd6f2c9ef620c8b21ecb01a369cc0ae9bb6c5
David Pulloquinga <dapulloqquinga@espe.edu.ec>
2018-01-20 17:30:48 -0500

    ok

--- masterkey/models.py
+++ masterkey/models.py
@@ -188,6 +188,16 @@ class Curso(models.Model):
         verbose_name = 'curso'
         verbose_name_plural = 'cursos'
 
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None, *args,**kwargs):
+        if self.capacidad_maxima <=0:
+            return
+        
+        else:
+            super(Curso, self).save( *args,**kwargs)
+
+
     def __str__(self):
         return "Sede:"+self.sede.nombre_sede+ "Fecha: " + str(self.fecha) + " Hora Inicio: " + str(self.hora_inicio) + \
                " Capacidad: " + str(self.capacidad_maxima)
